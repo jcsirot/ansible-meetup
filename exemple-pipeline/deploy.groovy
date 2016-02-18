556d48430720d8e3d0e0cc0919ab335a0e7882a3
Jean-Christophe Sirot <jcsirot@gmail.com>
2016-02-18 08:04:22 +0100

    Add exemple 3

---
+++ exemple-pipeline/deploy.groovy
@@ -0,0 +1,18 @@
+node {
+  git url: 'https://github.com/jcsirot/ansible-meetup.git'
+  dir('exemple-2') {
+    wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
+      ansiblePlaybook(
+        inventory: 'hosts.cfg',
+        playbook: 'deplopy.yml',
+        sudo: true,
+        credentialsId: '3356be1d-bf9b-4a15-b41f-5617808a3877',
+        extraVars: [
+          db_login: 'jcsirot',
+          db_pass: [value: '4K2Z_MZ+J;', hidden: true]
+        ]
+        colorized: true
+      )
+    }
+  }
+}
